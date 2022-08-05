35dcbc446c6dc327dfcf7ad1b6ebf3d5ac89cd76
LukyMDI <lucas.limaxs.l@gmail.com>
2022-08-04 21:06:19 -0300

    0.0.1

--- Script.js
+++ Script.js
@@ -13,11 +13,22 @@ document.addEventListener("scroll", function(){
 });
 //-----------Animação do segundo background----------//
 var bg2 = document.getElementById('bg-2');
+var txtbg2 = document.getElementById('txt-bg2');
 
 document.addEventListener("scroll", function(){
     var posicaoy = window.pageYOffset;
-    if(posicaoy >= 100){
+    if(posicaoy >= 120){
         bg2.style.animation = "bg2-anim 1.5s";
+//---------------Animação do texto-bg2-------------------//
+        if(posicaoy >= 650){
+            txtbg2.style.animation = "bg2-anim 0.8s";
+            txtbg2.style.transition = "none";
+            txtbg2.style.opacity = "1";
+        }else if(posicaoy < 580){
+            txtbg2.style.animation = "none";
+            txtbg2.style.transition = "opacity 0.8s";
+            txtbg2.style.opacity = "0";
+        }
     }else{
         bg2.style.animation = "none";
     }
